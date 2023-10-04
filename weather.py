0f293a3c4558d6019793940d84205cfb2c83b70c
mehnazaneef <mehnazaneef@gmail.com>
2023-10-04 22:37:43 +0530

    first commit

---
+++ weather.py
@@ -0,0 +1,23 @@
+#Weather API Application
+import requests
+
+API_KEY = "d073103a99eb550d70cc58ff0190da2b"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    
+    weather = data['weather'][0]['description']
+    temp = round(data['main']['temp'] - 273.15, 2)
+    
+    print("Weather: ",weather)
+    print("Temperature: ", temp, "celsius")
+    
+else:
+    print("An error occured")
